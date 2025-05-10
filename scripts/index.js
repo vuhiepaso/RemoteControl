449e35fa150f20611307725570d2ddd99911d621
hiepvd <vuhiepomg@gmail.com>
2025-05-10 23:37:25 +0700

    feat: init project

--- scripts/index.js
+++ scripts/index.js
@@ -13,25 +13,34 @@ const sequence = {
 
 // Kết nối Bluetooth qua cổng serial
 connectButton.addEventListener('click', async () => {
+    // try {
+    //     port = await navigator.serial.requestPort(); // Chọn cổng Bluetooth (COM)
+    //     await port.open({ baudRate: 9600 }); // Baudrate mặc định của Bluetooth
+    //     output.value += 'Đã kết nối Bluetooth\n';
+    //     // Thiết lập writer để gửi dữ liệu
+    //     writer = port.writable.getWriter();
+    //     // Đọc dữ liệu nhận được
+    //     const decoder = new TextDecoderStream();
+    //     const readableStreamClosed = port.readable.pipeTo(decoder.writable);
+    //     const reader = decoder.readable.getReader();
+    //     // Lắng nghe dữ liệu từ Bluetooth
+    //     while (true) {
+    //         const { value, done } = await reader.read();
+    //         if (done) break;
+    //         output.value += `Nhận: ${value}\n`;
+    //     }
+    // } catch (error) {
+    //     output.value += `Lỗi: ${error.message}\n`;
+    // }
     try {
-        port = await navigator.serial.requestPort(); // Chọn cổng Bluetooth (COM)
-        await port.open({ baudRate: 9600 }); // Baudrate mặc định của Bluetooth
-        output.value += 'Đã kết nối Bluetooth\n';
-        // Thiết lập writer để gửi dữ liệu
-        writer = port.writable.getWriter();
-        // Đọc dữ liệu nhận được
-        const decoder = new TextDecoderStream();
-        const readableStreamClosed = port.readable.pipeTo(decoder.writable);
-        const reader = decoder.readable.getReader();
-        // Lắng nghe dữ liệu từ Bluetooth
-        while (true) {
-            const { value, done } = await reader.read();
-            if (done) break;
-            output.value += `Nhận: ${value}\n`;
-        }
+        const device = await navigator.bluetooth.requestDevice({
+            acceptAllDevices: true,
+            optionalServices: ['battery_service']
+        });
     } catch (error) {
         output.value += `Lỗi: ${error.message}\n`;
     }
+    console.log(device.name);
 });
 
 // Gửi dữ liệu tới Bluetooth
